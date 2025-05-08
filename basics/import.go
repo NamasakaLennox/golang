b569e06697f6d488693e68b33da321aedf37801b
NamasakaLennox <namasakalennox254@gmail.com>
2025-05-08 14:28:56 +0300

    named imports

---
+++ basics/import.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello, Go Standard Lib")
+
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println("HTTP Response Status: ", resp.Status)
+}
