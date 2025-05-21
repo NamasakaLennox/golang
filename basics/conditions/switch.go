9a736a02f82636f36a85b323bbc0af2c12cf9735
NamasakaLennox <namasakalennox254@gmail.com>
2025-05-21 15:45:48 +0300

    loops and conditional statements

---
+++ basics/conditions/switch.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// switch expression {
+	// case val1:
+	// 	// code
+	// case val2:
+	// 	// code
+	// default:
+	// 	// code
+	// }
+
+	// you don't have to mention break in go unlike other languages
+	// switch x.(type) // type based evaluation
+
+}
